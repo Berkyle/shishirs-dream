5a4fb73f8170f2a59c586249ecbf188f4cbe5b80
David Li <david.li@cerner.com>
2019-10-03 14:16:46 -0500

    bootstrap table DONE

--- src/Components/ViewRoom.js
+++ src/Components/ViewRoom.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
+import Table from 'react-bootstrap/Table';
+import Image from 'react-bootstrap/Image'
 // eslint-disable-next-line
 import Col from 'react-bootstrap/Col';
 import '../App.css';
@@ -8,19 +10,27 @@ class ViewRoom extends React.Component {
   render() {
     return(
       <div>
-        <Row className="heading-color">
-          <h1>
-            View Playlist
-          </h1>
-        </Row>
-        
-        <div className="body-color">
-          <Row>
-            <h2> 
-              Hi
-            </h2>
-          </Row>
-        </div>
+        <h1>
+          View Playlist
+        </h1>
+        <Table responsive striped borderless hover variant="dark">
+          <thead>
+            <tr>
+              <th>&#128247;</th>
+              <th>Title </th>
+              <th>Artist</th>
+              <th>Length</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td><Image src="public/logo192.png" thumbnail /></td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>1</td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
     )
   }
