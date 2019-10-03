48404c54732cec7072b5cfe0a49941ae10292282
Shishir Lakkadi <shishirlakkadi@Shishirs-MacBook-Air.local>
2019-10-03 12:03:40 -0500

    view_playlist added css to heading

--- src/Components/ViewRoom.js
+++ src/Components/ViewRoom.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 // eslint-disable-next-line
 import Col from 'react-bootstrap/Col';
@@ -8,13 +7,21 @@ import '../App.css';
 class ViewRoom extends React.Component {
   render() {
     return(
-      <Container>
-        <Row className="justify-content-center">
+      <div>
+        <Row className="heading-color">
           <h1>
             View Playlist
           </h1>
         </Row>
-      </Container>
+        
+        <div className="body-color">
+          <Row>
+            <h2> 
+              Hi
+            </h2>
+          </Row>
+        </div>
+      </div>
     )
   }
 }
