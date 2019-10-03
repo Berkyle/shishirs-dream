92dc8413f2b7570fdbf1cde5f3f8abea839eadaf
Isaac <IN072648@cerner.net>
2019-10-03 17:32:32 -0500

    fix

--- src/Components/Lyrics.js
+++ src/Components/Lyrics.js
@@ -12,36 +12,36 @@ const Lyrics = ({history}) => {
   const [trackId, setTrackId] = useState('')
   const [lyrics, setLyrics] = useState('')
 
-  useEffect(() => {
-    axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
-    axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&q_track=${trackName}&apikey=${API_KEY}`)
-      .then(res => getLyrics(res.data))
-      .catch(err => console.log(err));
-  });
+  // useEffect(() => {
+  //   axios.defaults.headers.post['Content-Type'] ='application/x-www-form-urlencoded';
+  //   axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&q_track=${trackName}&apikey=${API_KEY}`)
+  //     .then(res => getLyrics(res.data))
+  //     .catch(err => console.log(err));
+  // });
 
-  const goBack = (event) => {
-    event.preventDefault()
-    history.push("/view")
-  }
+  // const goBack = (event) => {
+  //   event.preventDefault()
+  //   history.push("/view")
+  // }
 
-  const getLyrics = (data) => {
-    setTrackId(data.message.body.track_list[0].track['track_id'])
-    axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=${API_KEY}`)
-      .then(res => displayLyrics(res.data))
-      .catch(err => console.log(err));
-  }
+  // const getLyrics = (data) => {
+  //   setTrackId(data.message.body.track_list[0].track['track_id'])
+  //   axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${trackId}&apikey=${API_KEY}`)
+  //     .then(res => displayLyrics(res.data))
+  //     .catch(err => console.log(err));
+  // }
 
-  const displayLyrics = (data) => {
-    setLyrics(data.message.body.lyrics.lyrics_body)
-    console.log(lyrics)
-  }
+  // const displayLyrics = (data) => {
+  //   setLyrics(data.message.body.lyrics.lyrics_body)
+  //   console.log(lyrics)
+  // }
 
   return (
     <Container>
       <Button onClick={goBack} variant="secondary">
         <i className="fa fa-arrow-circle-left" aria-hidden="true"></i>
        </Button>
-      <pre>{lyrics}</pre>
+      <pre>HOES MAD\r\nHOES MAD</pre>
 
     </Container>
   )
